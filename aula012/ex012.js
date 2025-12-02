614abd2aae0b60c579e84f59df68e8b1f6079c1c
erik-marlon <erik1903marlon@gmail.com>
2025-12-02 17:27:00 -0300

    js02/12

---
+++ aula012/ex012.js
@@ -0,0 +1,9 @@
+var hora = 22
+if (hora < 12) {
+    console.log ("Bom dia!")
+} else if (hora => 12) {
+    console.log ("Boa tarde!")
+} else if (hora > 19 < 5){
+    console.log ("Boa noite!")
+}
+console.log ("Agora são exatamente ${hora} horas.")
---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade é ${vel}`)
+if (vel > 60) {
+    console.log(`Ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Siga em segurança!`)
