dd8450b5040612983e0ab918347a132920e61ad4
erik-marlon <erik1903marlon@gmail.com>
2025-12-03 14:17:50 -0300

    CONDIÇÕES

--- aula012/ex012.js
+++ aula012/ex012.js
@@ -1,9 +1,10 @@
-var hora = 22
+var agora = new Date ()
+var hora = agora.getHours()
 if (hora < 12) {
     console.log ("Bom dia!")
-} else if (hora => 12) {
+} else if (hora <= 18) {
     console.log ("Boa tarde!")
-} else if (hora > 19 < 5){
+} else {
     console.log ("Boa noite!")
 }
-console.log ("Agora são exatamente ${hora} horas.")
+console.log (`Agora são exatamente ${hora} horas.`)
