bb34d50c85585352f9d753bd15a0ff658b65ad30
erik-marlon <erik1903marlon@gmail.com>
2025-12-11 17:45:31 -0300

    calc

--- rascunho/app.js
+++ rascunho/app.js
@@ -1,17 +1,26 @@
-function calcular() {
-        let pot = Number(document.querySelector(".pot").value);
-        let carsaida = Number(document.querySelector(".carsaida").value);
-        let fpsaida = Number(document.querySelector(".fpsaida").value);
-        let inversor = Number(document.querySelector(".inversor").value);
-        let correntecatalogo = Number(document.querySelector(".correntecatalogo").value);
-        let tempocatalogo = Number(document.querySelector(".tempocatalogo").value);
-        // INPUTS DECLARADOS
-        let potenciaconsumida = document.querySelector(".potenciaconsumida")
-        //tensaobateria = document.querySelector('div#tensaobateria')
-        //correntedescarga = document.querySelector('div#correntedescarga')
-        //minutoscorrente = document.querySelector('div#minutoscorrente')
-        // RESULTADOS DECLARADOS
-        let valPot = pot * fpsaida * carsaida / inversor;
-        potenciaconsumida.innerText = valPot.toFixed(2);
-}
+function calcularPot() {
+        var Vpotnb = document.querySelector('input#potnb')
+        var VPcargasaida = document.querySelector('input#Pcargasaida')
+        var VFPsaida = document.querySelector('input#FPsaida')
+        var Vinversor = document.querySelector('input#inversor')
+        var res1 = document.querySelector('div#res1')
+        var res2 = document.querySelector('div#res2')
 
+        var potnb = Number(Vpotnb.value)
+        var Pcargasaida = Number(VPcargasaida.value) / 100
+        var FPsaida = Number(VFPsaida.value)
+        var inversor = Number(Vinversor.value) / 100
+
+        var VQbaterias = document.querySelector('input#Qbaterias')
+        var res3 = document.querySelector('div#res3')
+
+        var Qbaterias = Number(VQbaterias.value)
+
+        var Tensaobat = Qbaterias * 12
+        var Potconsumida = (potnb * Pcargasaida * FPsaida) / inversor
+        var Cdescarga = Potconsumida * 1000 / Tensaobat
+
+        res1.innerHTML = `<p>${Potconsumida.toFixed(2)}</p>`
+        res2.innerHTML = `<p>${Cdescarga.toFixed(2)}</p>`
+        res3.innerHTML = `<p>${Tensaobat}</p>`
+}
