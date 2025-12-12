5c81ad4e2fd838b9e0d47f3c9f006e9fc45df1f2
erik-marlon <erik1903marlon@gmail.com>
2025-12-12 16:40:22 -0300

    calc

--- rascunho/app.js
+++ rascunho/app.js
@@ -4,12 +4,17 @@ function calcularPot() {
         var FPsaida = Number(document.querySelector('#FPsaida').value)
         var inversor = Number(document.querySelector('#inversor').value) / 100
         var Qbaterias = Number(document.querySelector('#Qbaterias').value)
+        var correnteCAT = Number(document.querySelector('#correnteCAT').value)                      
+        var tempoCAT = Number(document.querySelector('#tempoCAT').value)
+
 
         var Tensaobat = Qbaterias * 12
         var Potconsumida = (potnb * Pcargasaida * FPsaida) / inversor
         var Cdescarga = (Potconsumida * 1000) / Tensaobat
+        var minutoscorrente = (correnteCAT / Cdescarga) * tempoCAT
         
         document.querySelector('#valorRes1').innerText = Potconsumida.toFixed(2) + " W"
         document.querySelector('#valorRes2').innerText = Tensaobat + " V"
         document.querySelector('#valorRes3').innerText = Cdescarga.toFixed(2) + " A"
+        document.querySelector('#valorRes4').innerText = minutoscorrente.toFixed(2)
 }
