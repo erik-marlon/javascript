1fbe2731858ff762645e20073294bfee31481284
erik-marlon <erik1903marlon@gmail.com>
2025-12-12 14:17:32 -0300

    calc

--- rascunho/app.js
+++ rascunho/app.js
@@ -1,21 +1,13 @@
 function calcularPot() {
-        var Vpotnb = document.querySelector('input#potnb')
-        var VPcargasaida = document.querySelector('input#Pcargasaida')
-        var VFPsaida = document.querySelector('input#FPsaida')
-        var Vinversor = document.querySelector('input#inversor')
-
-        var potnb = Number(Vpotnb.value)
-        var Pcargasaida = Number(VPcargasaida.value) / 100
-        var FPsaida = Number(VFPsaida.value)
-        var inversor = Number(Vinversor.value) / 100
-
-        var VQbaterias = document.querySelector('input#Qbaterias')
-
-        var Qbaterias = Number(VQbaterias.value)
+        var potnb = Number(document.querySelector('#potnb').value)
+        var Pcargasaida = Number(document.querySelector('#Pcargasaida').value) / 100
+        var FPsaida = Number(document.querySelector('#FPsaida').value)
+        var inversor = Number(document.querySelector('#inversor').value) / 100
+        var Qbaterias = Number(document.querySelector('#Qbaterias').value)
 
         var Tensaobat = Qbaterias * 12
         var Potconsumida = (potnb * Pcargasaida * FPsaida) / inversor
-        var Cdescarga = Potconsumida * 1000 / Tensaobat
+        var Cdescarga = (Potconsumida * 1000) / Tensaobat
         
         document.querySelector('#valorRes1').innerText = Potconsumida.toFixed(2) + " W"
         document.querySelector('#valorRes2').innerText = Tensaobat + " V"
